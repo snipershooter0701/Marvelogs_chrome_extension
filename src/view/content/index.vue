388ed518ef84d63ceb427a14a95b79f2d7d1f53a
snipershooter <snipershooter0701@gmail.com>
2023-07-25 23:02:19 +0300

    detail page highlight

--- src/view/content/index.vue
+++ src/view/content/index.vue
@@ -16,7 +16,7 @@ export default {
 
   mounted() {
     this.index();
-    this.borderHighlight();
+    this.handlerClickEvent();
   },
   unmounted() {
 
@@ -27,35 +27,64 @@ export default {
     };
   },
   methods: {
-
-    index() {
-      document.addEventListener('mouseup', (event) => {
-
-        const highlightedItems = document.querySelectorAll('.highlighted');
-        if (highlightedItems.length > 0) {
-          highlightedItems.forEach(item => {
-            item.classList.remove('highlighted');
-          });
-        }
-
-        const items = document.querySelectorAll('.' + event.target.className);
-        // Add an event listener to each item
-        items.forEach(item => {
-          item.classList.add('highlighted');
-        });
-
+    handlerClickEvent() {
+      const _this = this;
+      document.addEventListener('click', (event) => {
+        _this.highlightItems(event.target);
+        _this.getXPathResult(event.target);
       });
     },
 
-    borderHighlight() {
+    getXPathResult(element) {
+      const xpath = this.getXPathChild(element);
+      alert(xpath);
+
+    },
+
+    getXPathChild(child) {
+      if (child) {
+        const parentElement = child.parentNode;
+        if (parentElement) {
+          const index = Array.from(parentElement.children).indexOf(child);
+          const xpath = this.parseClassName(parentElement.className, child.className, index);
+          return xpath;
+        }
+      }
+    },
+
+    parseClassName(parent, child, index) {
+      const parseParentClassName = parent.replace(' ', '.');
+      const parseChildClassName = child.replace(' ', '.');
+      const parse_xpath = `.${parseParentClassName} .${parseChildClassName}[${index}]`;
+      return parse_xpath;
+    },
+
+    checkXPathCount(xpath) {
+      const items = document.querySelectorAll(xpath);
+      return items.length;
+    },
+
+    highlightItems(element) {
+      const highlightedItems = document.querySelectorAll('.highlighted');
+      if (highlightedItems.length > 0) {
+        highlightedItems.forEach(item => {
+          item.classList.remove('highlighted');
+        });
+      }
+
+      element.classList.add('highlighted');
+    },
+
+    index() {
       document.addEventListener('mouseover', (event) => {
         event.target.classList.add('border-yellow');
       });
-
+  
       document.addEventListener('mouseout', (event) => {
         event.target.classList.remove('border-yellow');
       });
     }
+
   },
   watch: {},
 };
